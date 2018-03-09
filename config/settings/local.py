4440bb5629c4fae75988274408b20b5f571345f3
Andy P <amp205@medschl.cam.ac.uk>
2018-03-09 10:54:15 +0000

    Added in qualtrics css as temporary measure And added ngrok to allowed_hosts in local

--- config/settings/local.py
+++ config/settings/local.py
@@ -63,3 +63,4 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 # Your local stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0def73f7.ngrok.io']
